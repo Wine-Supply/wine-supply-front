e85ea9ea63bcfdad5fa53fdf347b3b49359626f7
ChaconEfrain <efrachaga@gmail.com>
2022-11-21 20:37:38 -0600

    Instalación de @types/styled-components y primer diseño del footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,48 @@
-export function Footer(){
+import { FooterStyled } from "./FooterStyled";
 
+export default function Footer() {
+  return (
+    <FooterStyled>
+      <div className="footer-info">
+        <div className="social-copyright">
+          <div className="social-icons">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/efra%C3%ADn-chac%C3%B3n-521485234/"
+              className="social-icon"
+            >
+              FB
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/efra%C3%ADn-chac%C3%B3n-521485234/"
+              className="social-icon"
+            >
+              IG
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/efra%C3%ADn-chac%C3%B3n-521485234/"
+              className="social-icon"
+            >
+              TT
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/efra%C3%ADn-chac%C3%B3n-521485234/"
+              className="social-icon"
+            >
+              YT
+            </a>
+          </div>
+          <p className="copyright">&copy; 2022 Wine Supply, Inc</p>
+        </div>
+        <div className="business-info">BUSINESS INFO</div>
+      </div>
+    </FooterStyled>
+  );
 }
