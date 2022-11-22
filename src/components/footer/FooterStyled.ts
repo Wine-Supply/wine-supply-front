87cbfea332787fe130a3c04cd2dc5303ab657e76
ChaconEfrain <efrachaga@gmail.com>
2022-11-22 11:33:51 -0600

    Renderizado de los íconos de redes sociales y comportamiento responsivo; instalación de paquete @ant-design/icons

--- src/components/footer/FooterStyled.ts
+++ src/components/footer/FooterStyled.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
-  background-color: #141414;
-  color: #fdfdfd;
-  font-size: 1.6rem;
-  padding: 2rem 4rem;
+  background-color: var(--black);
+  color: var(--white);
+  font-size: 1.2rem;
+  padding: 2rem 2rem;
+  transition: all 0.3s;
 
   .footer-info {
     display: flex;
@@ -18,8 +19,39 @@ export const FooterStyled = styled.footer`
     gap: 1rem;
   }
 
+  .business-info {
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
+  }
+
   .social-icons {
     display: flex;
     gap: 1rem;
+    font-size: 1.6rem;
+  }
+
+  .social-icon:link,
+  .social-icon:visited {
+    display: inline-block;
+    color: var(--white);
+    transition: all 0.3s;
+  }
+
+  .social-icon:hover,
+  .social-icon:active {
+    transform: scale(1.2);
+  }
+
+  @media (min-width: 600px) {
+    font-size: 1.4rem;
+    padding: 2rem 4rem;
+    transition: all 0.3s;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 1.4rem;
+    padding: 2rem 6rem;
+    transition: all 0.3s;
   }
 `;
