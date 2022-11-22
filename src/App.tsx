2bc36ed8b1db20ea836e12c47015d24870f765f0
ChaconEfrain <efrachaga@gmail.com>
2022-11-22 15:08:37 -0600

    Componente 'about-us' responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Footer from "./components/footer/Footer";
+import AboutUs from "./components/section-about-us/AboutUs";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <AboutUs />
       <Footer />
     </div>
   );
