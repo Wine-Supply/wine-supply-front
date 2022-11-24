f426f524e3c01e06fe98a44532ef4b7451368365
ChaconEfrain <efrachaga@gmail.com>
2022-11-24 13:26:10 -0600

    Agregué una interface para el objeto vino

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,31 +1,53 @@
-import { GET_WINES } from '../actions/index'
+import { GET_TOP_RATED_WINES, GET_WINES } from "../actions/index";
 
-interface Actions{
-  type: string,
-  payload?:Array<Object>
+interface Wine {
+  id: string;
+  name: string;
+  brand: string;
+  description: string;
+  images: Array<string>;
+  alcoholVolume: number;
+  rating: number;
+  price: number;
 }
 
-export interface State{
-  allWines?:Array<Object>
+interface Actions {
+  type: string;
+  payload: Array<Wine>;
+}
+
+export interface State {
+  allWines: Array<Wine>;
+  topRatedWines: Array<Wine>;
 }
 
 const initialState = {
-  allWines:[]
+  allWines: [],
+  topRatedWines: [],
 };
 
-const rootReducer = (state:State= initialState, action: Actions) => {
+const rootReducer = (state: State = initialState, action: Actions) => {
   switch (action.type) {
     case GET_WINES:
-      console.log('rex', action.payload)
-      return{
+      console.log("rex", action.payload);
+      return {
         ...state,
-        allWines: action.payload
-      }
-  
+        allWines: action.payload,
+      };
+
+    case GET_TOP_RATED_WINES:
+      const topRated = [...state.allWines!]
+        .sort((a, b) => b.rating - a.rating)
+        .slice(0, 3);
+      return {
+        ...state,
+        topRatedWines: topRated,
+      };
+
     default:
-     return {
-      ...state
-    };
+      return {
+        ...state,
+      };
   }
 };
 
