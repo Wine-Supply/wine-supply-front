134774e10a60e00d97bfdabaab250ab0794f5cb3
ChaconEfrain <efrachaga@gmail.com>
2022-12-02 12:42:34 -0600

    login obligatorio para hacer compras implementado

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,45 +1,63 @@
-
-import { Link } from 'react-router-dom'
-import {CardContainer, CardInformation, ButtonAddCart} from './CardStyle'
+import { Link } from "react-router-dom";
+import { CardContainer, CardInformation, ButtonAddCart } from "./CardStyle";
 
 interface CardProps {
-    _id: string
-    name: string
-    img: string
-    descriptions: string
-    price: number
-    rating: number
-    addStorageItem?:any
+  _id: string;
+  name: string;
+  img: string;
+  descriptions: string;
+  price: number;
+  rating: number;
+  addStorageItem?: any;
+  token?: string | null;
+  setShowModal?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Card: React.FC<CardProps> = ({_id, name, img, descriptions, price, rating, addStorageItem}) => {
+const Card: React.FC<CardProps> = ({
+  _id,
+  name,
+  img,
+  descriptions,
+  price,
+  rating,
+  addStorageItem,
+  token,
+  setShowModal,
+}) => {
+  const handleAddItemToCart = () => {
+    if (token?.length === 0 && setShowModal) {
+      setShowModal(true);
+    } else addStorageItem(_id, name, img, descriptions, price, rating);
+  };
 
   return (
     <CardContainer>
-      <div className='imageMain' >
+      <div className="imageMain">
         <Link to={`detail/${_id}`}>
           <img src={img} alt={name} />
         </Link>
       </div>
       <CardInformation>
-        <div className='rankingStyle'>
+        <div className="rankingStyle">
           <p>{rating}/5</p>
         </div>
 
         <h2>{name}</h2>
-        
-        <div className='descriptionStyle'>
+
+        <div className="descriptionStyle">
           <span>Description</span>
-          <p>{descriptions.substring(0,30)}...</p>
+          <p>{descriptions.substring(0, 30)}...</p>
         </div>
 
-        <div className='cardFooter'>
+        <div className="cardFooter">
           <p>${price}</p>
-          <ButtonAddCart onClick={()=>addStorageItem(_id, name, img, descriptions, price, rating)} >add to cart</ButtonAddCart>
+          <ButtonAddCart onClick={handleAddItemToCart}>
+            add to cart
+          </ButtonAddCart>
         </div>
-      </CardInformation>      
+      </CardInformation>
     </CardContainer>
-  )
-}
+  );
+};
 
 export default Card;
