f43e31b5afe0dcc6254235be9ad4fe90e95f5dc7
Sol Diessler <sol.diessler@gmail.com>
2022-12-13 22:24:08 -0300

    dashboard, custom menu

--- src/components/admin/CustomStyle.js
+++ src/components/admin/CustomStyle.js
@@ -1,10 +1,15 @@
-import { styled } from '@mui/system';
-import { Datagrid } from 'react-admin';
+import { Menu } from 'react-admin';
+import WineBarIcon from '@mui/icons-material/WineBar';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
-export const MyDatagrid = styled(Datagrid)({
 
-  "& .RaDatagrid-headerCell": {
-    // backgroundColor: "var(--primary-color)",
-    // color:"var(--font-color)"
-  },
-});
+
+export const MyMenu = () => (
+  <Menu>
+      <Menu.DashboardItem />
+      <Menu.Item  to="/admin/admin/wines" primaryText="Wines" leftIcon={<WineBarIcon />}/>
+      <Menu.Item to="/admin/admin/users" primaryText="Users" leftIcon={<PersonOutlineIcon />}/>
+      <Menu.Item to="/admin/newsletter" primaryText="Newsletter" leftIcon={<MailOutlineIcon />}/>  
+  </Menu>
+);
